b08a2543d317bf26930361cef2eb0a38ba3a1b52
Wizard-F <forrest.futao.wei@gmail.com>
2021-12-08 12:18:14 +0800

    finished emptySpaceExists, maxTileExists, atLeastOneMoveExists

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -5,7 +5,7 @@ import java.util.Observable;
 
 
 /** The state of a game of 2048.
- *  @author TODO: YOUR NAME HERE
+ *  @author Forrest Futao Wei
  */
 public class Model extends Observable {
     /** Current contents of the board. */
@@ -137,7 +137,13 @@ public class Model extends Observable {
      *  Empty spaces are stored as null.
      * */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
+        for (int col=0; col<b.size(); col+=1) {
+            for (int row=0; row<b.size(); row+=1) {
+                if (b.tile(col, row) == null || b.tile(col, row).value() == 0) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -147,7 +153,13 @@ public class Model extends Observable {
      * given a Tile object t, we get its value with t.value().
      */
     public static boolean maxTileExists(Board b) {
-        // TODO: Fill in this function.
+        for (int col=0; col<b.size(); col+=1) {
+            for (int row=0; row<b.size(); row+=1) {
+                if (b.tile(col, row) != null && b.tile(col, row).value() == MAX_PIECE) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -158,7 +170,21 @@ public class Model extends Observable {
      * 2. There are two adjacent tiles with the same value.
      */
     public static boolean atLeastOneMoveExists(Board b) {
-        // TODO: Fill in this function.
+        if (emptySpaceExists(b)) return true;
+        for (int col=0; col<=b.size()-2; col+=1) {
+            for (int row=0; row<=b.size()-1; row +=1) {
+                if (b.tile(col, row).value() == b.tile(col+1, row).value()) {
+                    return true;
+                }
+            }
+        }
+        for (int row=0; row<=b.size()-2; row+=1) {
+            for (int col=0; col<=b.size()-1; col+=1) {
+                if (b.tile(col, row).value() == b.tile(col, row+1).value()) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
